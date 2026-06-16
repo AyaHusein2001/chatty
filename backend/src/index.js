f83ca34f2b5e4c5394f37ea28476d1f3237752fd
AyaHusein2001 <aya.ahmed2001.aa@gmail.com>
2026-06-16 12:12:19 +0300

    Add Dockerfile and .dockerignore; implement static file serving in Express

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,33 @@ import cors from "cors";
 import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import "dotenv/config";
+
+
+import fs from "fs";
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+const publicDir = path.join(process.cwd(), "public");
+
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
+
+
+// if the public directory exists, serve the static files
+// this is for the production build(deployment only)
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+  //redirect any route that is not an API route to the index.html file in the public directory to allow the frontend to handle the routing(react router)
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });

dddb2c4c934120a47afe8724134d90bd87f8ca71
AyaHusein2001 <aya.ahmed2001.aa@gmail.com>
2026-06-16 15:22:49 +0300

    Refactor webhook route and improve error handling for static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,8 +15,11 @@ const FRONTEND_URL = process.env.FRONTEND_URL;
 const publicDir = path.join(process.cwd(), "public");
 
 // it's important that you don't parse the webhook event data, it should be in the raw format
-app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
-
+app.use(
+  "/api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhook,
+);
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
@@ -27,8 +30,10 @@ app.use(clerkMiddleware());
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
   //redirect any route that is not an API route to the index.html file in the public directory to allow the frontend to handle the routing(react router)
-  app.get("/{*any}", (req, res, next) => {
-    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  app.get("*", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => {
+      if (err) next(err);
+    });
   });
 }
 
